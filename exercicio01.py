8c11c0ab092bf4e00b04b389af6e3da1cd8c639d
samuelthomeifsp <samuel.thome@aluno.ifsp.edu.br>
2025-10-17 11:30:49 -0300

    Arrumando

--- exercicio01.py
+++
@@ -1,8 +0,0 @@
-nome = input("Digite seu nome ")
-idade = int(input("Digite sua idade "))
-
-if idade < 18:
-    print(f"O {nome} é menor de idade")
-
-else:
-    print(f" O {nome} não é maior de idade")
